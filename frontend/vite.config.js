13cfef9e96b15fdae76ddfcd84c7ddc6d0cc13b0
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-07-26 20:37:26 +0300

    deploy frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/CargoFlow-Frontend/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
